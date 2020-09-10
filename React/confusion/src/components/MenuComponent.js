e6b1f1dbc892c0cfd1250fa34eac99fce2a13f63
LucianaSerrao <luliserrao@eng.ci.ufpb.br>
2020-09-10 18:12:27 -0300

    Components part 2

--- React/confusion/src/components/MenuComponent.js
+++ React/confusion/src/components/MenuComponent.js
@@ -19,18 +19,18 @@ class Menu extends Component {
         if(dish != null) {
             return(
                 <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImg top src={dish.image} alt={dish.name} />
                     <CardBody>
                         <CardTitle>{dish.name}</CardTitle>
                         <CardText>{dish.description}</CardText>
                     </CardBody>
                 </Card>
-            )
+            );
         }
         else {
             return(
                 <div></div>
-            )
+            );
         }
     }
 
@@ -38,9 +38,9 @@ class Menu extends Component {
         const menu = this.props.dishes.map((dish) => {
             return (
                 <div key={dish.id} className="col-12 col-md-5 mt-1">
-                    <Card onClick={() => this.onDishSelect(dish)}>                        
+                    <Card key={dish.id} onClick={() => this.onDishSelect(dish)}>                        
                         <CardImg width="100%" src={dish.image} alt={dish.name} />                        
-                        <CardImgOverlay body className="ml-5">
+                        <CardImgOverlay>
                             <CardTitle>{dish.name}</CardTitle>
                         </CardImgOverlay>
                     </Card>
@@ -52,14 +52,15 @@ class Menu extends Component {
             <div className="container">
                 <div className="row">
                     {menu} 
+                </div>            
+                <div className="row">
+                    <div className="col-12 col-md-5 mt-1">
+                        {this.renderDish(this.state.selectedDish)}
+                    </div>                
                 </div>
-            </div>
-            <div className="row">
-                {this.renderDish(this.state.selectedDish)}
             </div>
         );
     }
-
 }
 
 export default Menu;
