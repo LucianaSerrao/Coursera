98b712b332306f7c6186ded707c90186514f0d1f
LucianaSerrao <luliserrao@eng.ci.ufpb.br>
2020-09-19 12:49:51 -0300

    Week 2 Assignment

--- React/Week_2/confusion/src/components/MainComponent.js
+++ React/Week_2/confusion/src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import About from './AboutComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
@@ -48,6 +49,7 @@ class Main extends Component {
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
+          <Route exact path="/aboutus" component={About} />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
           <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
