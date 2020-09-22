afbaa0417286aabbde5294bc376f97c851444581
LucianaSerrao <luliserrao@eng.ci.ufpb.br>
2020-09-22 17:19:50 -0300

    React Redux Form Validation

--- React/Week_3/confusion/src/components/DishdetailComponent.js
+++ React/Week_3/confusion/src/components/DishdetailComponent.js
@@ -5,7 +5,6 @@ import { Link } from 'react-router-dom';
 function RenderDish({dish}) {
     if (dish != null) {
         return (
-            <div className='col-12 col-md-5 m-1'>
                 <Card>
                     <CardImg width="100%" src={dish.image} alt={dish.name} />
                     <CardBody>
@@ -13,7 +12,6 @@ function RenderDish({dish}) {
                         <CardText>{dish.description}</CardText>
                     </CardBody>
                 </Card>
-            </div>
         )
     }
     else {
@@ -44,12 +42,11 @@ function RenderComments({comments}) {
         }
     })
     return(
-        <div className='col-12 col-md-5 m-1'>
+        <div>
             <h4> Comments </h4>
             <ul className='list-unstyled'>
                 {comment_}
             </ul>
-
         </div>
     );
 }
@@ -58,7 +55,7 @@ function RenderComments({comments}) {
     const DishDetail = (props) => {
         if (props.dish != null) {
             return(
-                <div class="container">
+                <div className="container">
                     <div className="row">
                         <Breadcrumb>
                             <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
@@ -72,6 +69,8 @@ function RenderComments({comments}) {
                     <div className="row">
                         <div className="col-12 col-md-5 m-1">
                             <RenderDish dish={props.dish} />
+                        </div>
+                        <div className="col-12 col-md-5 m-1">
                             <RenderComments comments={props.comments} />
                         </div>
                     </div>
